a3b0795e0b09ab329b79fe1412f69a688dcfeb4a
nka21 <133028205+nka21@users.noreply.github.com>
2025-12-04 22:54:36 +0900

    👌

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,26 +19,6 @@ export const metadata: Metadata = {
     icon: "/icon.png",
     apple: "/apple-icon.png",
   },
-  openGraph: {
-    title: "nkoji",
-    description: "nkoji's website",
-    images: [
-      {
-        url: "/opengraph-image.png",
-        width: 1200,
-        height: 630,
-        alt: "nkoji",
-      },
-    ],
-    locale: "ja_JP",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "nkoji",
-    description: "nkoji's website",
-    images: ["/twitter-image.png"],
-  },
 };
 
 export default function RootLayout({
