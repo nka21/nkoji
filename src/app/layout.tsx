36d1bd56a1692151a58996a237e7c90cca8556f3
nka21 <133028205+nka21@users.noreply.github.com>
2025-12-04 21:59:44 +0900

    ✌️

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,30 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "nkoji",
   description: "nkoji's website",
+  icons: {
+    icon: "/icon.png",
+    apple: "/apple-icon.png",
+  },
+  openGraph: {
+    title: "nkoji",
+    description: "nkoji's website",
+    images: [
+      {
+        url: "/opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "nkoji",
+      },
+    ],
+    locale: "ja_JP",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "nkoji",
+    description: "nkoji's website",
+    images: ["/twitter-image.png"],
+  },
 };
 
 export default function RootLayout({
