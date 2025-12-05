ab058e46d7ce7679a70d1603e40aa09b28a130bf
nka21 <133028205+nka21@users.noreply.github.com>
2025-12-06 00:28:15 +0900

    💕

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col md:flex-row items-center justify-center min-h-screen gap-8 md:gap-24 px-4">
-      <h1 className="font-heading text-4xl md:text-6xl translate-y-0 md:translate-y-2 animate-slide-up order-2 md:order-1">nkoji</h1>
+      <h1 className="font-heading text-4xl md:text-6xl translate-y-0 md:translate-y-2 animate-slide-up animation-delay-text order-2 md:order-1">nkoji</h1>
       <Image
         src="/na.webp"
         alt="nkoji"
